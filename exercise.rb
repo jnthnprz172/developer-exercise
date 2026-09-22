1eecfe5fcb603aa040d22bcaca40941e43d86abc
Jonathan Perez <jpere172@ucsc.edu>
2026-09-21 19:43:15 -0700

    Complete Ruby marklar and even Fibonacci exercises

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,13 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    str.gsub(/[[:alpha:]]+/) do |word|
+      if word.length > 4
+        word.match?(/\A[[:upper:]]/) ? 'Marklar' : 'marklar'
+      else
+        word
+      end
+    end
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +18,15 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    current, following = 1, 1
+    total = 0
+
+    nth.times do
+      total += current if current.even?
+      current, following = following, current + following
+    end
+
+    total
   end
 
 end
